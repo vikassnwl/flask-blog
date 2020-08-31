9a6b25b3c32453125fe1feab93015c89b779de70
vikassnwl <viakssnwl@gmail.com>
2020-08-31 22:08:53 +0530

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 app = Flask(__name__)
+
+app.secret_key = 'unique secret key'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/blog'
 
@@ -26,6 +29,10 @@ class Posts(db.Model):
     slug = db.Column(db.String(), nullable=False)
     image = db.Column(db.String(), nullable=False)
     date = db.Column(db.DateTime(), nullable=False)
+
+
+with open('config.json') as c:
+    params = json.load(c)['params']
 
 
 @app.route('/')
@@ -62,17 +69,35 @@ def contact():
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        tagline = request.form.get('tagline')
-        content = request.form.get('content')
-        slug = request.form.get('slug')
-        image =request.form.get('image')
+    if 'user' in session and session['user'] == params['admin_user']:
 
-        entry = Posts(title=title, tagline=tagline, content=content, slug=slug, image=image, date=datetime.now())
-        db.session.add(entry)
-        db.session.commit()
-    return render_template('dashboard.html')
+        if request.method == 'POST':
+            title = request.form.get('title')
+            tagline = request.form.get('tagline')
+            content = request.form.get('content')
+            slug = request.form.get('slug')
+            image =request.form.get('image')
+
+            entry = Posts(title=title, tagline=tagline, content=content, slug=slug, image=image, date=datetime.now().strftime("%d %B %Y %I:%M %p"))
+            db.session.add(entry)
+            db.session.commit()
+        
+        posts = Posts.query.all()
+
+        return render_template('dashboard.html', posts=posts)
+    else:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == params['admin_user'] and password == params['admin_pass']:
+            session['user'] = params['admin_user']
+            return redirect("/dashboard")
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
---
+++ blog.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 31, 2020 at 06:32 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `sno` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `phone` text NOT NULL,
+  `message` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `sno` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `tagline` text NOT NULL,
+  `content` text NOT NULL,
+  `slug` text NOT NULL,
+  `image` text NOT NULL,
+  `date` text NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
